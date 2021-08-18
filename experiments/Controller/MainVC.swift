042174dfdb9d5bf4275fd44090f9e1b6881d3619
Mariah Baysic <mjpbaysic@pbcom.com.ph>
2021-08-18 15:40:59 +0800

    circle progress bar

--- experiments/Controller/MainVC.swift
+++ experiments/Controller/MainVC.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var circularProgress: CircularProgressView!
+    var progress: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        circularProgress.trackClr = UIColor.cyan
+        circularProgress.progressClr = UIColor.purple
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { timer in
+            if self.progress < 100 {
+                self.progress += 1
+                self.circularProgress.setProgressWithAnimation(duration: 1.0, value: self.progress)
+            }
+        }
     }
-
 
 }
 
